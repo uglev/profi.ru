5d2bd43d5a5d398652a8481ded5a72d39778ece6
uglev <46020541+uglev@users.noreply.github.com>
2024-03-20 09:50:33 +0300

    Add files via upload

---
+++ profi.py
@@ -0,0 +1,128 @@
+from bs4 import BeautifulSoup as bs
+import time
+from selenium import webdriver
+import smtplib
+from email.mime.text import MIMEText
+import telebot
+
+# Settings
+myLogin = 'yourlogin'  # login
+myPassword = 'pass'  # password
+main_key = {'Психология', 'обида', 'ремонт', 'трубы'}  # Needed words here
+
+token = '6489483666jkhkjjkhkjsdfhjkdshfjkhdskfjhdsh--yourtoken'
+chat_id = '@blablabla'
+
+url = 'https://profi.ru/backoffice/n.php'
+
+
+def send_email(task):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    me = '<your email (from)>'
+    you = '<your email (to)>'
+    server.login(me, '<your_email_password(from)>')
+    msg = MIMEText(task + url)
+    msg['Subject'] = 'Новое задание!'
+    msg['From'] = me
+    msg['To'] = you
+    server.sendmail(me, [you], msg.as_string())
+    server.quit()
+
+
+def refreshPage():
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            print("End of scroll")
+            break
+        last_height = new_height
+        print("New content!")
+    return
+
+
+bot = telebot.TeleBot(token)
+options = webdriver.ChromeOptions()
+options.add_argument(
+    "user-agent=Mozilla/5.0 (compatible; U; ABrowse 0.6; Syllable) AppleWebKit/420+ (KHTML, like Gecko)")
+
+driver = webdriver.Chrome(
+    options=options
+)
+
+# Enter login and password
+driver.get(url)
+namesLogin = driver.execute_script(
+    "return document.getElementsByClassName('ui-input ui-input-bo login-form__input-login ui-input_desktop ui-input_with-placeholder_empty');")
+namesLogin[0].clear()
+email_input = namesLogin[0].send_keys(myLogin)
+
+time.sleep(1)
+namesPassword = driver.execute_script(
+    "return document.getElementsByClassName('ui-input ui-input-bo login-form__input-password ui-input_desktop ui-input_with-placeholder_empty');")
+namesPassword[0].clear()
+password_input = namesPassword[0].send_keys(myPassword)
+time.sleep(1)
+
+namesButton = driver.execute_script(
+    "return document.getElementsByClassName('ui-button ui-button_mobile ui-button_mobile_large ui-button_font-size-18 ui-button_primary login-form__button');")
+button_input = namesButton[0].click()
+
+# Pause comfortably to enter the captcha
+time.sleep(30)
+
+page = driver.page_source
+
+soup = bs(page, 'html.parser')
+known_tasks = []
+
+try:
+    while True:
+        # Body of the loop after login
+        refreshPage()
+
+        arr = driver.execute_script(
+            "return document.getElementsByClassName('SnippetBodyStyles__Container-sc-br3c4b-0 dNCmqL');")
+
+        # We get a list of links of all profiles that we found, taking into account scrolling
+        # arrayClients = []
+        # for i in arr:
+        #     arrayClients.append(i.get_attribute('href'))
+        # There may be a collection of questionnaires or functionality as desired. arrayClients - list of urls
+
+        for block in soup.find_all(class_='bJoMKY'):
+            task_key = str(block.find(class_='bQJaTZ'))
+            name = str(block.find(class_='gYZUq'))
+            my_url = str(block.find(class_='dNCmqL'))
+            my_url = 'https://spb.profi.ru' + my_url[my_url.index('href="') + 6:my_url.index('" id')]
+            if '">' in task_key and '</h3>' in task_key and '">' in name and '</p>' in name:
+                task_stack = (task_key[task_key.index('">') + 2:task_key.index('</h3>')].lower(),
+                              name[name.index('">') + 2:name.index('</p>')].lower(), my_url)
+            else:
+                task_stack = tuple()
+
+            for key in main_key:
+                for i in task_stack:
+                    for j in i:
+                        if task_stack not in known_tasks and key.lower() in i.lower():
+                            # known_tasks.append(task_key)
+                            # If necessary, send by email
+                            # send_email(str(''.join(task_stack)))
+                            known_tasks.append(task_stack)
+                            bot.send_message(chat_id, str(': '.join(task_stack)))
+        time.sleep(60)
+        driver.get(url)
+        page = driver.page_source
+        soup = bs(page, 'html.parser')
+
+except Exception as ex:
+    print(ex)
+
+finally:
+    driver.close()
+    driver.quit()
